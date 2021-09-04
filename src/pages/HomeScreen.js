b50b53097f50b28214cae92efbcbe521e84aaeda
cristian mosquera <cmosqbone1@gmail.com>
2021-09-04 10:52:03 +0200

    add pages and templates

---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,22 @@
+// create template
+
+const HomeScreen = () => {
+    const view = `
+
+        <div class="Characthers">
+
+            <article class="Character-item">
+               <a href="#/1/">
+                    <img src="image" alt="name"/>
+                    <h2>Name</h2>
+               </a> 
+            </article>
+        
+        </div>
+    
+    `;
+
+    return view;
+};
+
+export default Home;
